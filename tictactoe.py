f3af3b986ee61736cf7209e0f42ab164c356fc44
Alosh Bennett <aloshbennett@gmail.com>
2024-12-14 11:32:52 +0000

    moved the game logic to intializeGame and startGame functions

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,7 @@
 import random 
 import time
 
+# Function declarations
 def printgrid():
     for i in range(9):
         print( cells[i], " ", ".", " ", end="")
@@ -48,53 +49,58 @@ def getMoveAI() :
         if (cells[move-1] != "x" and cells[move-1] != "o") :
             return move
 
-playersAI = [False, False]
-mode = int(input("how many players? : "))
-match mode :
-    case 0:
-        playersAI = [ True , True]
-    case 1 :
-        playerschoose = input("chooseplayer_select x or o : ")
-        if playerschoose == "x" :
-            playersAI = [False, True]
-        elif playerschoose == "o" :
-            playersAI = [True ,False]
-        else :
+def initializeGame():
+    global playersAI
+    mode = int(input("how many players? : "))
+    match mode :
+        case 0:
+            playersAI = [ True , True]
+        case 1 :
+            playerschoose = input("chooseplayer_select x or o : ")
+            if playerschoose == "x" :
+                playersAI = [False, True]
+            elif playerschoose == "o" :
+                playersAI = [True ,False]
+            else :
+                print("Invalid entry")
+                exit(1)
+        case 2 :
+            playersAI= [False, False]
+        case _ :
             print("Invalid entry")
             exit(1)
-    case 2 :
-        playersAI= [False, False]
-    case _ :
-        print("Invalid entry")
-        exit(1) 
 
-
-
-cells = [ "1" , "2" ,"3" , "4" ,"5" , "6" ,"7" , "8" ,"9" ,  ]
-players = [ "x" , "o" ]
-printgrid()
-winning= False
-count = 0
-
-while not winning :
-    p_index = count%2
-    player = players[p_index]
-    print ("ready player ", player )
-    move = getMove(playersAI[p_index])
-
-    if (move == 0):
-        print("enter again")
-        continue
-    if  cells[move-1] == "x" or cells[move-1] == "o" :
-        print("NO CHEATING >:( ")
-        continue
-    cells[move-1] = player
+def startGame() :
     printgrid()
-    if win() :
-        print("congratulations player ", player)
-        winning = "yes"
-    elif draw() :
-        print("it'a a draw!")
-        winning = "yes"
+    winning= False
+    count = 0
 
-    count = count + 1
+    while not winning :
+        p_index = count%2
+        player = players[p_index]
+        print ("ready player ", player )
+        move = getMove(playersAI[p_index])
+
+        if (move == 0):
+            print("enter again")
+            continue
+        if  cells[move-1] == "x" or cells[move-1] == "o" :
+            print("NO CHEATING >:( ")
+            continue
+        cells[move-1] = player
+        printgrid()
+        if win() :
+            print("congratulations player ", player)
+            winning = "yes"
+        elif draw() :
+            print("it'a a draw!")
+            winning = "yes"
+
+        count = count + 1
+
+# Script execution
+playersAI = [False, False]
+cells = ["1", "2", "3", "4", "5", "6", "7", "8", "9", ]
+players = ["x", "o"]
+initializeGame()
+startGame()
